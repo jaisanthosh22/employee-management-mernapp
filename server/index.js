aa4373b8bc1d055c04c647f16bd99aca85cbaa4b
jai santhosh <jai2201santhosh@gmail.com>
2026-01-02 11:35:42 +0530

    feat: configure environment variables using dotenv

--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
 //using express
 
 const express = require("express")
+const dotenv = require("dotenv");
+
+dotenv.config()
 const app = express()
 
 
+const port= process.env.PORT 
+app.listen(port,()=>{
+    console.log(`port is running sucessfully ${port}`)
+})
